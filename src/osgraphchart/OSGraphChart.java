c812267ece9429dbf7e91ce5660ee12b8bd9d43c
devin-olson <54080725+devin-olson@users.noreply.github.com>
2021-07-27 16:52:48 -0500

    I committed to the wrong project,so all of the current code will be transferred and deleted.

--- src/osgraphchart/OSGraphChart.java
+++ src/osgraphchart/OSGraphChart.java
@@ -11,19 +11,41 @@ package osgraphchart;
  */
 public class OSGraphChart {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        
-      /*  Module02 mod2 = new Module02();
+        //Retrieves the current system time in milliseconds
+        long startTime = System.currentTimeMillis();
+      
+      /* MODULE02 ASSIGNMENT 
+       *   Module02 mod2 = new Module02();
        * 
        *  mod2.StackOperations();
-      */
+       */
       
       Module03 mod3 = new Module03();
+      System.out.println("Selection Sort: \n");
+      mod3.fillArr(1000);
+      mod3.selectionSort();
       
-      mod3.fillArr();
+      System.out.println();
+      System.out.println("\n------------------------------------------------------------------------");
+      System.out.println("\nBubble Sort: \n");
+      mod3.fillArr(1000);
+      mod3.bubbleSort();
+      
+      
+      
+      
+      
+      
+     
+      //Calculates the amount of time the compiler takes to execute the code 
+      //written in milliseconds
+      long stopTime = System.currentTimeMillis();
+      long runTime = stopTime - startTime;
+      System.out.println("\n\nRun time: " + runTime);
+      
+      
     }
     
 }

1e91f0f692841a40cfd5d034ba5e29733e9199de
devin-olson <54080725+devin-olson@users.noreply.github.com>
2021-07-27 12:19:37 -0500

    Goal: create two sort and search algorithms. I started by creating an array to hold 100 random numbers between 1-1000. I then created a function to populate the array with random numbers.

---
+++ src/osgraphchart/OSGraphChart.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osgraphchart;
+
+/**
+ *
+ * @author Devin
+ */
+public class OSGraphChart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      /*  Module02 mod2 = new Module02();
+       * 
+       *  mod2.StackOperations();
+      */
+      
+      Module03 mod3 = new Module03();
+      
+      mod3.fillArr();
+    }
+    
+}
